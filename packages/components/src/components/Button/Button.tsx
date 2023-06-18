e18b567e96a938c405a8fd07827f69409042d1e9
murtaza ahmad <murtazaahmad3003@gmail.com>
2023-06-18 17:00:45 +0500

    Button...

--- packages/components/src/components/Button/Button.tsx
+++ packages/components/src/components/Button/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export interface ButtonProps {}
 
 const Button: React.FC<ButtonProps> = ({}) => {
-  return <button>Button</button>;
+  return <button>Button one</button>;
 };
 
 export default Button;
